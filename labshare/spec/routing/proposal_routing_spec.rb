ac8830695e5fd24c9f59a668cef220aba51835da
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 14:59:08 -0500

    write proposal route specs

---
+++ labshare/spec/routing/proposal_routing_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Routing to proposals", :type => :routing do
+  it "routes GET /proposals to proposals#index" do
+    expect(:get => "/proposals").to route_to("proposals#index")
+  end
+
+  it "routes GET /proposals/new to proposals#new" do
+    expect(:get => "/proposals/new").to route_to("proposals#new")
+  end
+
+  it "routes GET /proposals/show to proposals#show" do
+    expect(:get => "/proposals/show").to route_to("proposals#show")
+  end
+
+  it "routes POST /proposals to proposals#create" do
+    expect(:post => "/proposals").to route_to("proposals#create")
+  end
+
+  it "routes GET /proposals/1/edit to proposals#edit" do
+    expect(:get => "/proposals/1/edit").to route_to("proposals#edit")
+  end
+
+end
