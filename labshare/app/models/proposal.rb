a7c98990dabe8611c06f58ab38e40f22b77fc925
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 16:03:35 -0500

    write model tests for proposal attributes

---
+++ labshare/app/models/proposal.rb
@@ -0,0 +1,3 @@
+class Proposal < ActiveRecord::Base
+
+end
--- labshare/spec/models/proposals_spec.rb
+++ labshare/spec/models/proposals_spec.rb
@@ -3,23 +3,32 @@ require 'date'
 
 describe Proposal do
   let(:proposal) { Proposal.new }
+  proposal_details = {name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), status: "open", proposer_id: 2}
 
   describe "proposal" do
     it "has a name" do
-      proposal = Proposal.create(name: "Proposal 1", hypothesis: "It gets figures stuff out", completion_date: Date.new(2016,10,03), status: "open", proposer: 2)
+      proposal = Proposal.create(proposal_details)
       expect(proposal.name).to eq "Proposal 1"
     end
 
     it "has a hypothesis" do
+      proposal = Proposal.create(proposal_details)
+      expect(proposal.hypothesis).to eq "It figures stuff out"
     end
 
     it "has a completion date" do
+      proposal = Proposal.create(proposal_details)
+      expect(proposal.target_completion_date).to be_a(Date)
     end
 
     it "has a status" do
+      proposal = Proposal.create(proposal_details)
+      expect(proposal.status).to eq "open"
     end
 
     it "has a proposer" do
+      proposal = Proposal.create(proposal_details)
+      expect(proposal.proposer_id).to eq 2
     end
   end
 end
