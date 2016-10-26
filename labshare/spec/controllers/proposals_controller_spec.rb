c11981046158c69cbad25c8e5e15812ef5d14c2c
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 16:12:25 -0500

    add controller tests for proposal GET routes

--- labshare/spec/controllers/proposals_controller_spec.rb
+++ labshare/spec/controllers/proposals_controller_spec.rb
@@ -1,1 +1,23 @@
-proposals_controller_spec.rb
+require 'rails_helper'
+
+describe ProposalController do
+  let!(:proposal) { Proposal.create!(name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), status: "open", proposer_id: 2) }
+
+  describe "GET #index" do
+    it "responds with status code 200" do
+      get :index
+      expect(response).to have_http_status 200
+    end
+
+    it "assigns the all proposals as @proposals" do
+      get :index
+      expect(assigns(:proposals)).to eq(Proposal.all)
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+end
