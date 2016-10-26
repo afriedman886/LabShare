2470ba1a6189daeae24bb927c2a187b9a9d2e431
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 17:13:26 -0500

    add/pass tests for proposal controller

--- labshare/spec/controllers/proposals_controller_spec.rb
+++ labshare/spec/controllers/proposals_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe ProposalsController do
-  let!(:proposal) { Proposal.create!(name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), status: "open", proposer_id: 2) }
+  let!(:proposal) { Proposal.create!(name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), proposer_id: 2) }
 
   describe "GET #index" do
     it "responds with status code 200" do
@@ -37,92 +37,70 @@ describe ProposalsController do
     end
   end
 
-  # describe "GET #new" do
-  #   it "responds with status code 200" do
-  #     get :new
-  #     expect(response).to have_http_status 200
-  #   end
+  describe "GET #new" do
+    it "responds with status code 200" do
+      get :new
+      expect(response).to have_http_status 200
+    end
 
-  #   it "assigns a new proposal to @proposal" do
-  #     get :new
-  #     expect(assigns(:proposal)).to be_a_new Proposal
-  #   end
+    it "assigns a new proposal to @proposal" do
+      get :new
+      expect(assigns(:proposal)).to be_a_new Proposal
+    end
 
-  #   it "renders the :new template" do
-  #     get :new
-  #     expect(response).to render_template(:new)
-  #   end
-  # end
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 
-  # describe "POST #create" do
-  #   context "when valid params are passed" do
-  #     it "responds with status code 302" do
-  #       post :create, { proposal: { user_throw: "rock" } }
-  #       expect(response).to have_http_status 302
-  #     end
+  proposal_details = {name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03)}
 
-  #     it "creates a new proposal in the database" do
-  #       expect {post :create, { proposal: { user_throw: "rock" } }}.to change {Proposal.all.count}.by(1)
-  #     end
+  describe "POST #create" do
 
-  #     it "assigns the newly created proposal as @proposal" do
-  #       post :create, { proposal: { user_throw: "paper" } }
-  #       expect(assigns(:proposal)).to eq Proposal.last
-  #     end
+    context "when valid params are passed" do
+      it "responds with status code 302" do
+        post :create, { proposal: proposal_details }
+        expect(response).to have_http_status 302
+      end
 
-  #     it "sets a notice that the proposal was successfully created" do
-  #       post :create, { proposal: { user_throw: "paper" } }
-  #       expect(flash[:notice]).to eq 'Game was successfully created.'
-  #     end
+      it "creates a new proposal in the database" do
+        expect {post :create, { proposal: proposal_details }}.to change {Proposal.all.count}.by(1)
+      end
 
-  #     it "redirects to the created game" do
-  #       post :create, { game: { user_throw: "rock" } }
-  #       expect(response).to redirect_to(game_path(Game.last))
-  #     end
-  #   end
+      it "assigns the newly created proposal as @proposal" do
+        post :create, { proposal: proposal_details }
+        expect(assigns(:proposal)).to eq Proposal.last
+      end
 
-  #   context "when invalid params are passed" do
-  #     it "responds with status code 200" do
-  #       post :create, { game: { user_throw: "pineapple" } }
-  #       expect(response).to have_http_status 200
-  #     end
+      it "sets a notice that the proposal was successfully created" do
+        post :create, { proposal: proposal_details }
+        expect(flash[:notice]).to eq 'Proposal was successfully created.'
+      end
 
-  #     it "does not create a new game in the database" do
-  #       expect { post :create, { game: { user_throw: "pineapple" } } }.not_to change{Game.all.count}
-  #     end
+      it "redirects to the created proposal" do
+        post :create, { proposal: proposal_details }
+        expect(response).to redirect_to(proposal_path(Proposal.last))
+      end
+    end
 
-  #     it "assigns the unsaved game as @game" do
-  #       post :create, { game: { user_throw: "paper" } }
-  #       expect(assigns(:game)).to eq Game.last
-  #     end
+    invalid_proposal_details = {hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03)}
 
-  #     it "renders the :new template" do
-  #       post :create, { game: { user_throw: "pineapple" } }
-  #       expect(response).to render_template(:new)
-  #     end
-  #   end
-  # end
+    context "when invalid params are passed" do
+      it "responds with status code 200" do
+        post :create, { proposal: invalid_proposal_details }
+        expect(response).to have_http_status 200
+      end
 
-  # describe "DELETE #destroy" do
-  #   it "responds with status code 302" do
-  #     delete :destroy, { id: game.id }
-  #     expect(response).to have_http_status 302
-  #   end
+      it "does not create a new game in the database" do
+        expect { post :create, { proposal: invalid_proposal_details } }.not_to change{Proposal.all.count}
+      end
 
-  #   it "destroys the requested game" do
-  #     expect { delete(:destroy, { id: game.id }) }.to change(Game, :count).by(-1)
-  #   end
-
-  #   it "sets a notice that the game was destroyed" do
-  #     delete :destroy, { id: game.id }
-  #     expect(flash[:notice]).to match /Game was successfully destroyed/
-  #   end
-
-  #   it "redirects to the games list" do
-  #     delete :destroy, { id: game.id }
-  #     expect(response).to redirect_to games_url
-  #   end
-  # end
-
+      it "renders the :new template" do
+        post :create, { proposal: invalid_proposal_details }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 
 end
