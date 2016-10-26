da884b168ea83ebb5d003172dffba604e19f43fe
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 17:13:46 -0500

    add/pass spec tests for proposal model

--- labshare/spec/models/proposals_spec.rb
+++ labshare/spec/models/proposals_spec.rb
@@ -3,7 +3,7 @@ require 'date'
 
 describe Proposal do
   let(:proposal) { Proposal.new }
-  proposal_details = {name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), status: "open", proposer_id: 2}
+  proposal_details = {name: "Proposal 1", hypothesis: "It figures stuff out", target_completion_date: Date.new(2016,12,03), proposer_id: 2}
 
   describe "proposal" do
     it "has a name" do
@@ -23,7 +23,7 @@ describe Proposal do
 
     it "has a status" do
       proposal = Proposal.create(proposal_details)
-      expect(proposal.status).to eq "open"
+      expect(proposal.status).to eq "Open"
     end
 
     it "has a proposer" do
