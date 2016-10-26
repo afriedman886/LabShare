ee6f99931ee02543e7a9aa27143e0ba0eca01bc6
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-26 17:12:49 -0500

    add routes for show and new proposals

--- labshare/app/controllers/proposals_controller.rb
+++ labshare/app/controllers/proposals_controller.rb
@@ -6,4 +6,27 @@ class ProposalsController < ApplicationController
   def show
     @proposal = Proposal.find_by(id: params[:id])
   end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    # switch and test this line once user model/controller is accessible
+    # @proposal = current_user.proposals.new(proposal_params)
+    @proposal = Proposal.new(proposal_params)
+
+    # hard coding user id for testing, switch when user model/controller is accessible
+    @proposal.assign_attributes(proposer_id: 2)
+    if @proposal.save
+      redirect_to @proposal, notice: 'Proposal was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+  def proposal_params
+    params.require(:proposal).permit(:name, :hypothesis, :target_completion_date)
+  end
 end
