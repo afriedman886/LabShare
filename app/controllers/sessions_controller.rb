c039caeca6e0018f1a63e73f169ece1b8f78185f
Amy Friedman & Greg Garner <pair+afriedman886.garner412@devbootcamp.com>
2016-10-28 14:56:48 -0500

    change errors partial and surround files that utilize it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to new_session_path
+      @error = ["Email and password combination not found."]
+      render new_session_path
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "User successfully created."
     else
-      @errors = @user.errors
       render :new
     end
   end
